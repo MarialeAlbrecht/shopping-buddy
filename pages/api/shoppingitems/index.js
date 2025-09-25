bd17fd2515a36574e3f9edc5566b88205884090a
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-25 16:02:27 +0200

    added try/catch error

--- pages/api/shoppingitems/index.js
+++ pages/api/shoppingitems/index.js
@@ -4,10 +4,17 @@ import Item from "@/db/models/shoppingitems";
 export default async function handler(request, response) {
   await dbConnect();
 
-  if (request.method === "GET") {
-    const items = await Item.find();
-    response.status(200).json(items);
+  try {
+    if (request.method === "GET") {
+      const items = await Item.find();
+      response.status(200).json(items);
+      return;
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: "Internal server error" });
     return;
   }
+
   response.status(405).json({ status: "Method not allowed" });
 }
