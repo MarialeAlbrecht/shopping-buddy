085bd36861d66943a6d724009c4b39cb7bc68e2d
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-25 16:31:20 +0200

    fixed category schema

--- db/models/category.js
+++ db/models/category.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const categoriesSchema = new Schema({
-  category: { type: String, required: true },
-});
+const categoriesSchema = new Schema(
+  {
+    category: { type: String, required: true },
+  },
+  { collection: "Category" }
+);
 
 const Category =
   mongoose.models.Category || mongoose.model("Category", categoriesSchema);
