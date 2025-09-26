0af05d5f590efd6dfd5a351e1453fb835c00676c
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-26 11:11:59 +0200

    added the css styles per category

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,9 +1,36 @@
+import styled, { css } from "styled-components";
+
 export default function ProductCard({ name, quantity, category }) {
+  const categoryColors = {
+    Dairy: "pink",
+    Bakery: "wheat",
+    Fruits: "lightgreen",
+    Vegetables: "darkgreen",
+    Meat: "salmon",
+    Beverages: "lightgray",
+    Snacks: "skyblue",
+    Household: "lightyellow",
+    "Personal Care": "purple",
+    Other: "red",
+  };
+
+  const color = categoryColors[category];
+
   return (
     <>
-      <p>Name: {name}</p>
-      <p>Quantity: {quantity}</p>
-      <p>Category: {category}</p>
+      <Card $categoryColor={color}>
+        <p>Name: {name}</p>
+        <p>Quantity: {quantity}</p>
+        <p>Category: {category}</p>
+      </Card>
     </>
   );
 }
+
+const Card = styled.div`
+  ${({ $categoryColor }) =>
+    $categoryColor &&
+    css`
+      background-color: ${$categoryColor};
+    `}
+`;
