71decc394e35b99f494bae605cde7cd98b309096
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-26 10:30:54 +0200

    added the conditional if the list is empty

--- components/ProductList.js
+++ components/ProductList.js
@@ -18,16 +18,20 @@ export default function ProductList() {
     <>
       <h1>Here is your shopping list</h1>
       <h2>You have {data.length} products in total.</h2>
-      <ul>
-        {data.map((item) => (
-          <ProductCard
-            key={item._id}
-            name={item.name}
-            quantity={item.quantity}
-            category={item.category}
-          />
-        ))}
-      </ul>
+      {data.length === 0 ? (
+        <h1>Your shoopping list is empty, please add new products.</h1>
+      ) : (
+        <ul>
+          {data.map((item) => (
+            <ProductCard
+              key={item._id}
+              name={item.name}
+              quantity={item.quantity}
+              category={item.category}
+            />
+          ))}
+        </ul>
+      )}
     </>
   );
 }
