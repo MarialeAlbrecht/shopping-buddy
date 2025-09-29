4acc4b72da56e20c20ea47fc7372a28e2e7377b9
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-29 10:57:11 +0200

    created form

---
+++ components/ProductForm.jsx
@@ -0,0 +1,41 @@
+import useSWR from "swr";
+
+export default function ProductForm({}) {
+  const { data, isLoading, error } = useSWR("/api/categories");
+
+  function handleSubmit(event) {
+    const formData = new FormData(event.target);
+    const data = Object.formEntries(formData);
+
+    console.log("NewProdutc", data);
+  }
+
+  if (isLoading) {
+    return <p>Loading form...</p>;
+  }
+  if (error) {
+    return <p>Failed to load categories...</p>;
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add a new product:</h1>
+      <label htmlFor="name">Add item name:</label>
+      <input type="text" id="name" name="name" required />
+      <label htmlFor="quantity">Select the quantity:</label>
+      <input type="number" id="quantity" name="quantity" required />
+      <label htmlFor="category">Select a category:</label>
+      <select id="category" name="category" required>
+        <option>Categories </option>
+        {data.map((category) => (
+          <option key={category._id} value={category.category}>
+            {category.category}
+          </option>
+        ))}
+      </select>
+      <label htmlFor="comment">Add a comment:</label>
+      <input type="text" id="comment" name="comment" />
+      <button type="submit">Add Product</button>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import ProductCard from "@/components/ProductCard";
 import ProductList from "@/components/ProductList";
+import ProductForm from "@/components/ProductForm";
 
 export default function HomePage() {
   return (
     <div>
+      <ProductForm />
       <ProductList />
     </div>
   );
