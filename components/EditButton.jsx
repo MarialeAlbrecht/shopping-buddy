c83263e2cbbb0efe819147faf01d3c446613fc6c
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-09-30 14:41:42 +0200

    created styled component for the edit button

---
+++ components/EditButton.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const EditButton = styled.button`
+  background-color: #6aca10ff;
+  border-radius: 6px;
+  font-size: small;
+`;
+
+export default EditButton;
