ffc3fa4f77bf73c981dea3488e1d27575d6fd082
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-10-02 10:00:07 +0200

    added local storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import { useEffect } from "react";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
-  const [bookmark, setBookmark] = useState([]);
+  const [bookmark, setBookmark] = useLocalStorageState("bookmarks", {
+    defaultValue: [],
+  });
+
+  useEffect(() => {
+    console.log("bookmarkschanged", bookmark);
+  }, [bookmark]);
 
   function handleToggleBookmark(id) {
     setBookmark((prev) =>
