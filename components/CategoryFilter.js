c5d7c9bd3daecebd8deae334962d63cee552deed
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-10-06 10:20:16 +0200

    added category component

---
+++ components/CategoryFilter.js
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+export default function CategoryFilter({ onSelectCategory }) {
+  const { data: categories, isLoading, error } = useSWR("api/categories");
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>We could not load the categories...</p>;
+  }
+
+  return (
+    <div>
+      {categories.map((category) => (
+        <div
+          key={category._id}
+          onClick={() => onSelectCategory(category.category)}
+        >
+          <span>{category.emoji}</span>,<span>{category.category}</span>,
+        </div>
+      ))}
+    </div>
+  );
+}
