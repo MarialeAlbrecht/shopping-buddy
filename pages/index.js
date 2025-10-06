86098b4da348d85afee7c4def84b0de0240d4442
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-10-06 10:24:07 +0200

    addedcategories to index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import ProductList from "@/components/ProductList";
 import AddProductButton from "@/components/AddProductButton";
+import { useState } from "react";
+import CategoryFilter from "@/components/CategoryFilter";
 
 export default function HomePage({ bookmark, onToggleBookmark }) {
+  const [selectCategory, setSelectCategory] = useState(null);
   return (
     <div>
       <AddProductButton />
-      <ProductList bookmark={bookmark} onToggleBookmark={onToggleBookmark} />
+      <CategoryFilter onSelectCategory={setSelectCategory} />
+      <ProductList
+        bookmark={bookmark}
+        onToggleBookmark={onToggleBookmark}
+        selectCategory={selectCategory}
+      />
     </div>
   );
 }
