7851f1600e50845b52e8c9dd8e35f55d4a6533c3
Maria Alejandra Albrecht <mariale.albrecht@gmail.com>
2025-10-09 12:43:30 +0200

    added recipe page to the nav bar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,19 +4,25 @@ import Link from "next/link";
 import Image from "next/image";
 import list from "@/assets/list.png";
 import purchased from "@/assets/purchased.png";
+import vegetables from "@/assets/vegetables.png";
 
 export default function Navbar() {
   const router = useRouter();
   return (
     <Nav>
-      <NavItem active={router.pathname === "/"}>
+      <NavItem>
         <Link href="/">
           <Image src={list} alt="Shopping List" width={45} height={45} />
         </Link>
       </NavItem>
-      <NavItem active={router.pathname === "/purchaseditems"}>
+      <NavItem>
         <Link href="/purchaseditems">
           <Image src={purchased} alt="Purchased List" width={45} height={45} />
+        </Link>
+      </NavItem>
+      <NavItem>
+        <Link href="/recipe">
+          <Image src={vegetables} alt="Recipes page" width={58} height={58} />
         </Link>
       </NavItem>
     </Nav>
@@ -31,7 +37,7 @@ const Nav = styled.nav`
   bottom: 0;
   left: 0;
   width: 100%;
-  padding: 0.6rem 0;
+  padding: 0.4rem 0;
   z-index: 2;
   color: white;
 `;
@@ -41,4 +47,7 @@ const NavItem = styled.div`
   flex: 1;
   justify-content: center;
   align-items: center;
+  &:not(:last-child) {
+    border-right: 1.5px solid white;
+  }
 `;
